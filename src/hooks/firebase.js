54e53bbb926d0be77a2df36d83a30be3fc5eec6f
ProstoBig <lordlaveshek@gmail.com>
2024-10-31 23:17:30 +0200

    added new api key to gitignore

--- src/hooks/firebase.js
+++ src/hooks/firebase.js
@@ -12,11 +12,12 @@ import { auth , db as database } from '../firebase';
 const firebaseConfig = {
     apiKey: "AIzaSyBwNV5s6abEszR_X2g-aoP7tf5zPQrkk4Y",
     authDomain: "quizness-2f9b9.firebaseapp.com",
+    databaseURL: "https://quizness-2f9b9-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "quizness-2f9b9",
-    storageBucket: "quizness-2f9b9.appspot.com",
+    storageBucket: "quizness-2f9b9.firebasestorage.app",
     messagingSenderId: "262313680925",
-    appId: "1:262313680925:web:53b1ee596afed78614a6f4",
-    measurementId: "G-L10SX7VNWH"
+    appId: "1:262313680925:web:79fcc8285345421014a6f4",
+    measurementId: "G-PDHFVJ1WM4"
 };
 
 // Initialize Firebase
